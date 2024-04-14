900dda5e454b4fab2eef213129e6f1fa6fea7fc0
gooney <gooney.me@gmail.com>
2024-04-14 22:23:34 +0800

    Add superAdmin field to Role model and create SuperAdmin model

---
+++ src/server/db/enhance.ts
@@ -0,0 +1,14 @@
+
+import type { GetServerSidePropsContext } from 'next';
+import { currentUser } from '@/lib/auth';
+import { enhance } from '@zenstackhq/runtime';
+import { db } from '../db';
+
+
+export async function getEnhancedPrisma() {
+  const user = await currentUser();
+  return enhance(
+    db,
+    { user },
+  );
+}
