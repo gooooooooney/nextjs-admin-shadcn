5c2ef1ed06ea9e15f63c191db4cb45d689b4ec8a
gooney <gooney.me@gmail.com>
2024-04-17 16:00:03 +0800

    Update layout and styling in components

--- src/components/layout/sidebar/index.tsx
+++ src/components/layout/sidebar/index.tsx
@@ -28,7 +28,7 @@ export const Sidebar = ({ routes }: SidebarProps) => {
     <div
       id="docs-sidebar"
       className={cn(
-        'bg-background hidden border-e  pb-10 pt-4 transition-all lg:relative lg:inset-y-0 lg:z-50 lg:flex lg:w-72 lg:flex-col ',
+        'bg-muted/20 h-full hidden border-e  pb-10 pt-4 transition-all lg:relative lg:inset-y-0 lg:z-50 lg:flex lg:w-72 lg:flex-col ',
         {
           'lg:w-20': collapsed,
         }
@@ -45,10 +45,10 @@ export const Sidebar = ({ routes }: SidebarProps) => {
       </div>
       <SidebarMenu {...{ pathname, routes, collapsed, setCollapsed }} />
       <Button
-        variant="ghost"
+        variant="outline"
         size="icon"
         onClick={() => setCollapsed(!collapsed)}
-        className="bg-background absolute -right-3 top-5 z-10 size-6 rounded-full shadow-lg"
+        className="bg-background absolute -right-3 top-1/2 -translate-y-1/2 z-10 size-6 rounded-full shadow-lg"
       >
         <Icons.ChevronLeft
           className={cn('size-4 transition-transform', {
--- src/components/ui/data-table/data-table.tsx
+++ src/components/ui/data-table/data-table.tsx
@@ -40,6 +40,8 @@ interface DataTableProps<TData, TValue> {
    */
   floatingBar?: React.ReactNode | null
   title?: React.ReactNode
+
+  ref?: React.Ref<HTMLDivElement>
   description?: React.ReactNode
 }
 
@@ -47,8 +49,9 @@ export function DataTable<TData, TValue>({
   table,
   columns,
   title = "DataTable",
-  description =  "Description of the data table.",
+  description = "Description of the data table.",
   floatingBar = null,
+  ref
 }: DataTableProps<TData, TValue>) {
   return (
     <Card>
@@ -59,54 +62,56 @@ export function DataTable<TData, TValue>({
         </CardDescription>
       </CardHeader>
       <CardContent>
-        <Table>
-          <TableHeader>
-            {table.getHeaderGroups().map((headerGroup) => (
-              <TableRow key={headerGroup.id}>
-                {headerGroup.headers.map((header) => {
-                  return (
-                    <TableHead key={header.id}>
-                      {header.isPlaceholder
-                        ? null
-                        : flexRender(
-                          header.column.columnDef.header,
-                          header.getContext()
-                        )}
-                    </TableHead>
-                  )
-                })}
-              </TableRow>
-            ))}
-          </TableHeader>
-          <TableBody>
-            {table.getRowModel().rows?.length ? (
-              table.getRowModel().rows.map((row) => (
-                <TableRow
-                  key={row.id}
-                  data-state={row.getIsSelected() && "selected"}
-                >
-                  {row.getVisibleCells().map((cell) => (
-                    <TableCell key={cell.id}>
-                      {flexRender(
-                        cell.column.columnDef.cell,
-                        cell.getContext()
-                      )}
-                    </TableCell>
-                  ))}
+        <div ref={ref} className="overflow-auto h-[500px] relative">
+          <Table>
+            <TableHeader className="bg-muted sticky top-0 z-50 shadow-sm">
+              {table.getHeaderGroups().map((headerGroup) => (
+                <TableRow key={headerGroup.id}>
+                  {headerGroup.headers.map((header) => {
+                    return (
+                      <TableHead key={header.id}>
+                        {header.isPlaceholder
+                          ? null
+                          : flexRender(
+                            header.column.columnDef.header,
+                            header.getContext()
+                          )}
+                      </TableHead>
+                    )
+                  })}
                 </TableRow>
-              ))
-            ) : (
-              <TableRow>
-                <TableCell
-                  colSpan={columns.length}
-                  className="h-24 text-center"
-                >
-                  No results.
-                </TableCell>
-              </TableRow>
-            )}
-          </TableBody>
-        </Table>
+              ))}
+            </TableHeader>
+            <TableBody>
+              {table.getRowModel().rows?.length ? (
+                table.getRowModel().rows.map((row) => (
+                  <TableRow
+                    key={row.id}
+                    data-state={row.getIsSelected() && "selected"}
+                  >
+                    {row.getVisibleCells().map((cell) => (
+                      <TableCell key={cell.id}>
+                        {flexRender(
+                          cell.column.columnDef.cell,
+                          cell.getContext()
+                        )}
+                      </TableCell>
+                    ))}
+                  </TableRow>
+                ))
+              ) : (
+                <TableRow>
+                  <TableCell
+                    colSpan={columns.length}
+                    className="h-24 text-center"
+                  >
+                    No results.
+                  </TableCell>
+                </TableRow>
+              )}
+            </TableBody>
+          </Table>
+        </div>
       </CardContent>
       <CardFooter>
         <div className="flex flex-col gap-2.5 text-xs text-muted-foreground">
