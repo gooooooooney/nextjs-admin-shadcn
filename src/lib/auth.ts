8d85c092b91a4dee69dfb8bad0fe97680228b5aa
gooney <gooney.me@gmail.com>
2024-05-10 17:43:26 +0800

    Refactor auth.ts and add cache for currentUser function

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -2,11 +2,12 @@ import "server-only"
 
 import { auth } from "@/server/auth"
 import { getUserById } from "@/server/data/user"
+import { cache } from 'react'
 
-export const currentUser = async () => {
+export const currentUser = cache(async () => {
   const session = await auth()
   return session?.user
-}
+})
 
 export const getLatestUser = async () => {
   const user = await currentUser()
