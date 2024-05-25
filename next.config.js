3ec9844016f2b3b809c26d4c624d1dbf71c2f652
gooney <gooney.me@gmail.com>
2024-05-25 13:56:57 +0800

    feat: use react compiler

--- next.config.js
+++ next.config.js
@@ -1,8 +1,12 @@
-/**
- * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
- * for Docker builds.
- */
-await import("./src/env.js");
+
+import createJiti from "jiti";
+import path from "path";
+
+// This is validation for the environment variables early in the build process.
+const jiti = createJiti(new URL(import.meta.url).pathname);
+jiti(path.resolve(process.cwd(), "./src/env.js"))
+
+const isProd = process.env.NODE_ENV === "production";
 
 /** @type {import("next").NextConfig} */
 const config = {
@@ -14,6 +18,9 @@ const config = {
         hostname: "avatars.githubusercontent.com",
       }
     ]
+  },
+  experimental: {
+    reactCompiler: isProd,
   },
 
 };
